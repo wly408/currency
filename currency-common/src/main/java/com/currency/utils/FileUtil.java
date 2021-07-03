820c94a8bb259db034de8eb18f5e6b73f867f12d
wly408 <271883839@qq.com>
2021-07-03 10:37:22 +0800

    初始化

---
+++ currency-common/src/main/java/com/currency/utils/FileUtil.java
@@ -0,0 +1,4 @@
+package com.currency.utils;
+
+public class FileUtil {
+}
---
+++ currency-mgt-web/src/main/java/com/currency/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.currency.controller;
+
+import com.currency.utils.FileUtil;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+
+    public void test(){
+        FileUtil fileUtil = null;
+    }
+}
